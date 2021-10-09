beff7c60d6f839280a316b367a10fd8bb550067d
Neal Archival <archivalneal@gmail>
2021-10-09 01:39:43 -0700

    changes

--- counter.js
+++ counter.js
@@ -1,24 +1,36 @@
 var count = 0;
 var ctrTextElem = document.getElementById("counterDisplay");
+const timeDelayMS = 500; // 0.75 seconds
 
-function updateCounter() {
+function updateCounter(num, event) {
+    if(count === 10000 || count === -10000) return;
+    count += num;
+    switch(count) {
+        case(0):
+            document.getElementById("counterDisplay").style.color = 'white';
+            break;
+        case(1):
+            document.getElementById("counterDisplay").style.color = 'green';
+            break;
+        case(-1):
+            document.getElementById("counterDisplay").style.color = 'red';
+            break;
+    }
     ctrTextElem.innerHTML = count;
+    event.preventDefault();
 }
 
+
+
 function increaseCount() {
-    if(count === 0) document.getElementById("counterDisplay").style.color = 'green';
-    count++;
-    updateCounter();
+    updateCounter(1);
 }
 
 function decreaseCount() {
-    if(count === 0) document.getElementById("counterDisplay").style.color = 'red';
-    count--;
-    updateCounter();
+    updateCounter(-1);
 }
 
 function resetCount() {
-    document.getElementById("counterDisplay").style.color = 'white';
     count = 0;
-    updateCounter();
+    updateCounter(0);
 }
